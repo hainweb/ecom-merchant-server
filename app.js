54b2e48210ce550c09844433456d90657dc714b9
hain <hainofficialweb@gmail.com>
2025-07-15 20:26:35 +0530

    updated cors

--- app.js
+++ app.js
@@ -71,7 +71,8 @@ const sessionConfig = {
     httpOnly: true,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-    domain: process.env.NODE_ENV === 'production' ? 'https://king-cart-merchant.onrender.com' : undefined
+    domain: process.env.NODE_ENV === 'production' ? 'king-cart-merchant.onrender.com' : undefined
+
   }
 };
 
